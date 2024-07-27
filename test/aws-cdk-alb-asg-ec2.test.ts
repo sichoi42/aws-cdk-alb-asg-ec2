575eba39b326a3e6a5ac1bbc52ea440643a7472f
sichoi42 <42.4.sichoi@gmail.com>
2024-07-27 09:50:51 +0900

    Initial commit

---
+++ test/aws-cdk-alb-asg-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkAlbAsgEc2 from '../lib/aws-cdk-alb-asg-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-alb-asg-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkAlbAsgEc2.AwsCdkAlbAsgEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
